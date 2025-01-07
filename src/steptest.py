85decdda348ee4124f976e136c04d41422184ba0
Michael Karliner <mike@modern-industry.com>
2025-01-07 11:33:16 +0000

    CHANGE: Remove old code, do quarter turn each minute

--- src/steptest.py
+++ src/steptest.py
@@ -1,5 +1,4 @@
-# Rui Santos & Sara Santos - Random Nerd Tutorials
-# Complete project details at https://RandomNerdTutorials.com/raspberry-pi-pico-stepper-motor-micropython/
+
 
 import stepper
 import neopixel
@@ -58,23 +57,7 @@ try:
 except OSError as error:
     print("NTP Timeout")
     machine.reset()
-# wlan = network.WLAN(network.STA_IF) # create station interface
-# print("Created interface")
-# time.sleep(1)
-# wlan.active(False)       # activate the interface
-# print("Deactivated interface")
-# time.sleep(2)
-# wlan.active(True)
-# print("Activated interface")
-# wlan.config(txpower=5)
-# time.sleep(2)
-# print("Scanning")
-# wlan.scan()             # scan for access points
-# time.sleep(3)
-# wlan.isconnected()      # check if the station is connected to an AP
-# wlan.connect('mihome', 'electr1c') # connect to an AP
-# wlan.config('mac')      # get the interface's MAC address
-# wlan.ifconfig()  # get the interface's IPv4 addresses
+
 
 # Define the stepper motor pins
 IN1 = 21
@@ -84,26 +67,19 @@ IN4 = 9
 
 # Initialize the stepper motor
 stepper_motor = stepper.HalfStepMotor.frompins(IN1, IN2, IN3, IN4)
-stepper_motor.stepms = 10
-sleep_time = 1
+stepper_motor.stepms = 5
+sleep_time = 0.5
 columns = 6
 rows = 10
 row_height = 50
 reveal_height = 1000
 ndigits = 4
-steps = (4096-(rows*row_height)-reveal_height)
-
-# Set the current position as position 0
-# stepper_motor.reset()
-
+#steps = (4096-(rows*row_height)-reveal_height) # Does full rotate
+steps = 1000
 
 
 try:
     while True:
-#         if not (wlan.isconnected()):
-#             # check if the station is connected to an AP
-#             wlan.connect('mihome', 'electr1c') # connect to an AP
-#             print("Connected")
         gmt = localtime()
 
         hour = gmt[3]
@@ -116,15 +92,10 @@ try:
             for d in range(ndigits):
                 for c in range(columns):
                     cc = chars[digits[d]][r][c]
-#                     if cc == '#':
-#                         print( cc, end='')
-#                     else:
-#                         print(c, end='')
                     if chars[digits[d]][r][c] == '#':
                              setled(d, c, 1)
                     else:
                              setled(d, c, 0)
-#                     print(chars[digits[d]][r][c], end='')
             print()
             sleep(sleep_time)
             stepper_motor.step(row_height)
